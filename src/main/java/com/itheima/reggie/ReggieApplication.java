d62e509f8021a68b108720f90079fcc052526778
root <1216733856@qq.com>
2023-02-15 18:43:39 +0800

    Reggie FirstCommit

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -3,8 +3,11 @@ package com.itheima.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
+@ServletComponentScan
 @SpringBootApplication
 public class ReggieApplication {
     public static void main(String[] args) {
