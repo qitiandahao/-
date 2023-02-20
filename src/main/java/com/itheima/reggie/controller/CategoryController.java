c9c1abbae5a35b44b4ecca25ca729aefd9519f18
root <1216733856@qq.com>
2023-02-20 22:09:34 +0800

    Reggie FirstCommit

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
@@ -18,5 +20,27 @@ public class CategoryController {
         log.info("category:{}",category);
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+    @GetMapping("/page")
+    public R<Page>page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo=new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category>queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(Category::getSort);
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(Integer id){
+        log.info("删除分类，id为{}",id);
+        categoryService.removeById(id);
+        return R.success("分类信息删除成功");
     }
 }
