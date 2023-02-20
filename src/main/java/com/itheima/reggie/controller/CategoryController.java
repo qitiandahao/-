6a412937f7f768171427f01829aa791bda054854
root <1216733856@qq.com>
2023-02-20 21:01:40 +0800

    Reggie FirstCommit

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,15 +1,22 @@
 package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
-
+    public R<String> save(@RequestBody Category category){
+        log.info("category",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
 }
